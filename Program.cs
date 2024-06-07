cdbb23cc6a6a31cdb0c18de1887a7e3e6b87f57e
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-06-07 18:50:55 +0300

    IeltsSectionsAdded

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+StartProgram();
+static void StartProgram()
+{
+Console.WriteLine("Welcome to Ielts Calculator Program");
+Console.WriteLine("Welcome IELTS Band Score Calculator\nPlease enter your score");
+Console.Write("Listening Score: ");
+double listening = Convert.ToDouble(Console.ReadLine());
+Console.Write("Writing Score: ");
+double writing = Convert.ToDouble(Console.ReadLine());
+Console.Write("Reading Score: ");
+double reading = Convert.ToDouble(Console.ReadLine());
+Console.Write("Speaking Score: ");
+double speaking = Convert.ToDouble(Console.ReadLine());
+}
+
