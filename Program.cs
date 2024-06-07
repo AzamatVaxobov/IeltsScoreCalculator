881b79f0e6e4a2808c1e7e102f112ba1067c9624
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-06-07 19:20:40 +0300

    Ielts Calculator is finished

--- Program.cs
+++ Program.cs
@@ -15,6 +15,12 @@ Console.Write("Speaking Score: ");
 double speaking = Convert.ToDouble(Console.ReadLine());
 
 double overall = (listening+writing+reading+speaking)/4;
+if(overall > 8 && overall <=9) Console.WriteLine("Your Ielts level is Expert");
+if(overall > 7 && overall <=8) Console.WriteLine("Your Ielts level is Very Good");
+if(overall > 6 && overall <=7) Console.WriteLine("Your Ielts level is Good");
+if(overall > 5 && overall <=6) Console.WriteLine("Your Ielts level is Competent");
+if(overall == 5) Console.WriteLine("Your Ielts level is Modest ");
+if(overall > 9 && overall <5) Console.WriteLine("Below or above that range is Invalid");
 }
 
 
