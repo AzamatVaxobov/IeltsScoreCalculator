1646b59e7e89d02d514b367870e2dda9be1ca019
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-06-08 09:13:07 +0300

    Code Fixed

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ static void StartProgram()
     else if (overall >= 6.5 && overall < 7.5) Console.WriteLine("Your Ielts level is Good");
     else if (overall >= 5.5 && overall < 6.5) Console.WriteLine("Your Ielts level is Competent");
     else if (overall >=5 && overall < 5.5) Console.WriteLine("Your Ielts level is Modest ");
-    else if (overall > 9 && overall < 5) Console.WriteLine("Below or above that range is Invalid");
+    else if (overall > 9 || overall < 5) Console.WriteLine("Below or above that range is Invalid");
 }
 
 
