560304094908e62011567d94b15f50992b71a7c7
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-06-08 09:09:40 +0300

    Code Updated

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@
 StartProgram();
 static void StartProgram()
 {
+   
     Console.WriteLine("Welcome to Ielts Calculator Program");
-    Console.WriteLine("Welcome IELTS Band Score Calculator\nPlease enter your score");
+    
     Console.Write("Listening Score: ");
     double listening = Convert.ToDouble(Console.ReadLine());
     Console.Write("Writing Score: ");
@@ -15,12 +16,12 @@ static void StartProgram()
     double speaking = Convert.ToDouble(Console.ReadLine());
 
     double overall = (listening + writing + reading + speaking) / 4;
-    if (overall > 8 && overall <= 9) Console.WriteLine("Your Ielts level is Expert");
-    if (overall > 7 && overall <= 8) Console.WriteLine("Your Ielts level is Very Good");
-    if (overall > 6 && overall <= 7) Console.WriteLine("Your Ielts level is Good");
-    if (overall > 5 && overall <= 6) Console.WriteLine("Your Ielts level is Competent");
-    if (overall == 5) Console.WriteLine("Your Ielts level is Modest ");
-    if (overall > 9 && overall < 5) Console.WriteLine("Below or above that range is Invalid");
+    if (overall >= 8.5 && overall <= 9) Console.WriteLine("Your Ielts level is Expert");
+    else if (overall >=7.5 && overall < 8.5) Console.WriteLine("Your Ielts level is Very Good");
+    else if (overall >= 6.5 && overall < 7.5) Console.WriteLine("Your Ielts level is Good");
+    else if (overall >= 5.5 && overall < 6.5) Console.WriteLine("Your Ielts level is Competent");
+    else if (overall >=5 && overall < 5.5) Console.WriteLine("Your Ielts level is Modest ");
+    else if (overall > 9 && overall < 5) Console.WriteLine("Below or above that range is Invalid");
 }
 
 
